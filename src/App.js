05992d9391e963fa73a32c54104cbb2ae77bfef4
Gireesh Naidu <gireesh.testengineer@gmail.com>
2023-01-20 22:10:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Bar} from 'react-chartjs-2';
+import {chart} from 'chart.js/auto'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const labels=['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sep','Oct','Nov','Dec'];
+const options={
+  plugins:{
+    legend:{
+      position:'top'
+    },
+    title:{
+      display:true,
+      text:'Expense Tracker'
+    }
+  }
 }
 
-export default App;
+const data={
+  labels,
+  datasets:[
+    {
+      label:'2020 Expense',
+      data:[10000,8000,12000,14000,9000,13000,12400,14000,9800,6700,7900,4500],
+      backgroundColor:'pink'
+    },
+    {
+      label:'2021 Expense',
+      data:[12000,8200,13000,12300,9600,12000,13400,15000,9100,6200,9700,5400],
+      backgroundColor:'orange'
+    }
+  ]
+}
+
+const App = () => {
+  return (
+    <div style={{'height':400,'width':800,'margin':'auto'}}>
+      <Bar options={options} data={data}/>
+    </div>
+  )
+}
+
+export default App
